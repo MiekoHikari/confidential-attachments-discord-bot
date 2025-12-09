4c6f986949bb14c09b934c2d8ea6c2de8203278f
Sheikh Showhanur Rahman <Showhanur@hotmail.com>
2025-12-09 22:59:24 +0400

    remove unused attachment announcement embed function

--- src/lib/services/cams.service.ts
+++ src/lib/services/cams.service.ts
@@ -1,18 +1,5 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageActionRowComponentBuilder } from 'discord.js';
 
-export function attachmentAnnounceEmbed(userId: string, length: number) {
-	return new EmbedBuilder()
-		.setColor('White')
-		.setDescription(
-			`### <@${userId}> has just uploaded ${length} file${length !== 1 ? 's' : ''}! \
-            \n> - Click the button(s) below to view the uploaded media. \
-            \n> - A watermark will be applied when you click the button. Generation may take a few moments depending on the file size. \
-            \n> - **Do not share these files outside of this server!**`
-		)
-		.setThumbnail('https://cdn3.emoji.gg/emojis/73057-anonymous.png')
-		.setTimestamp();
-}
-
 export const refreshButtonRow = (jobId: string) => new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(refreshButton(jobId));
 
 export const refreshButton = (jobId: string) =>
